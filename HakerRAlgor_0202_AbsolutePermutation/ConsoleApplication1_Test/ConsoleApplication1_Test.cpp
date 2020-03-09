26676bbab57e63eac454a2c415b8bf9cd86a1f08
sanghoon23 <sanghoon23@naver.com>
2020-03-09 15:29:58 +0900

    Update
    *0202 - AbsolutePermutation
    *0308 - BomberMan

---
+++ HakerRAlgor_0202_AbsolutePermutation/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,120 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+struct Pair
+{
+	bool Max = false;
+	int Result = 0;
+
+	Pair(bool max, int result)
+		: Max(max)
+		, Result(result) {}
+};
+
+// @ Vector - Result
+// @ k - K
+// @ Num - 해당 값.
+// @ Max - 해당 값이 최솟값인지, 최댓값인지.
+void Looping(vector<int>* Vector, int k, int Num, bool Max)
+{
+	if (Max == true && Num + k < Vector->size())
+	{
+		int Input = Num + k;
+		(*Vector)[Input - 1] = Input + k;
+		Input += k;
+		Looping(Vector, k, Input, Max);
+
+	}
+	else if (Max == false && Num - k > 0)
+	{
+		int Input = Num - k;
+		(*Vector)[Input - 1] = Input - k;
+		Input -= k;
+		Looping(Vector, k, Input, Max);
+	}
+}
+
+// Complete the absolutePermutation function below.
+vector<int>* absolutePermutation(int n, int k)
+{
+	std::vector<int>* In = new vector<int>(n);
+	for (int j = 0; j < n; ++j)
+		(*In)[j] = (j + 1);
+
+	if (k == 0) // K == 0 일 때,
+		return In;
+
+	// @ Fail
+	vector<int>* Fail = new vector<int>();
+	Fail->push_back(-1);
+
+	// @ Result
+	vector<int>* Result = new vector<int>(n, -1); // Result
+
+	// @ Calc
+	vector<Pair>* Calc = new vector<Pair>(); // Calc
+	for (int i = 0; i < Result->size(); ++i)
+	{
+		int Min = (i + 1) - k;
+		int Max = (i + 1) + k;
+		if (Min <= 0) // 0 보다 작을 때,
+		{
+			(*Result)[i] = Max;
+			Pair pair(true, Max);
+			Calc->push_back(pair);
+			continue;
+		}
+		else if (Max > n) // n 보다 클 때,
+		{
+			(*Result)[i] = Min;
+			Pair pair(false, Min);
+			Calc->push_back(pair);
+			continue;
+		}
+		else if (Max > n && Min <= 0) // 선택되지 않으면 제외.
+			return Fail;
+	}
+
+	for (int s = 0; s < Calc->size(); ++s)
+	{
+		bool Range = (*Calc)[s].Max;
+		Looping(Result, k, (*Calc)[s].Result, Range); // Looping (재귀)
+	}
+
+
+	// Check
+	for (int Num : *Result)
+	{
+		if (Num == -2 || Num > n || Num < 0)
+			return Fail;
+	}
+
+	return Result;
+}
+
+int main()
+{
+	int n = 10;
+	int k = 1;
+
+	vector<int> test = *(absolutePermutation(n, k));
+
+	printf("출력값");
+
+	for (int i = 0; i < test.size(); ++i)
+	{
+		printf("%d", test[i]);
+		
+		(i != test.size() - 1)
+			? printf(", ")
+			: printf("\n");
+	}
+
+	return 0;
+}
+
---
+++ HakerRAlgor_0308_BomberMan/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,138 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+struct DataPair
+{
+	int Colum = 0;
+	int Row = 0;
+};
+
+//@Make 4 way (up, down, left, right, self) to '.'
+void Search(int j, int i, vector<string>& grid, int ColumNum, int RowNum)
+{
+	grid[j][i] = '.';
+	if (j - 1 >= 0)
+	{
+		grid[j - 1][i] = '.';
+	}
+
+	if (j + 1 < ColumNum)
+	{
+		grid[j + 1][i] = '.';
+	}
+
+	if (i - 1 >= 0)
+	{
+		grid[j][i - 1] = '.';
+	}
+
+	if (i + 1 < RowNum)
+	{
+		grid[j][i + 1] = '.';
+	}
+}
+
+
+void Loop(vector<string>& grid, int Colum, int Row, int LoopNumber)
+{
+	vector<DataPair> Pairs;
+	Pairs.reserve(Colum * Row);
+
+	for (int Num = 0; Num < LoopNumber; ++Num)
+	{
+		//@Set Pair & All grid value make to 'O'
+		for (int j = 0; j < Colum; ++j)
+		{
+			for (int i = 0; i < Row; ++i)
+			{
+				if (grid[j][i] == 'O')
+				{
+					DataPair P;
+					P.Colum = j;
+					P.Row = i;
+					Pairs.emplace_back(P);
+				}
+
+				grid[j][i] = 'O';
+			}//for(i)
+		}//for(j)
+
+		 //@Calc Pair
+		for (int N = 0; N < Pairs.size(); ++N)
+		{
+			int J = Pairs[N].Colum;
+			int I = Pairs[N].Row;
+			Search(J, I, grid, Colum, Row);
+		}
+		Pairs.clear();
+
+	}//for(Num)
+}
+
+// Complete the bomberMan function below.
+vector<string> bomberMan(int n, vector<string> grid)
+{
+	// vector<string> Grid = grid;
+	int colum = grid.size();
+	int row = grid[0].size();
+
+	//@Initial,
+	if (n == 1)
+		return grid;
+
+	//@Full - 'O'
+	if (n % 2 == 0)
+	{
+		for (int j = 0; j < colum; ++j)
+		{
+			for (int i = 0; i < row; ++i)
+			{
+				grid[j][i] = 'O';
+			}//for(i)
+		}//for(j)
+
+		return grid;
+	}//if(n % 2 == 0)
+
+	//@터진 상황
+	if ((n - 3) % 4 == 0)
+	{
+		Loop(grid, colum, row, 1); //@Loop Once
+	}
+	else if ((n - 5) % 4 == 0)
+	{
+		Loop(grid, colum, row, 2); //@Loop Twice
+	}
+
+	return grid;
+}
+
+int main()
+{
+
+	string s1 = ".......";
+	string s2 = "...0...";
+	string s3 = "....0..";
+	string s4 = ".......";
+	string s5 = "00.....";
+	string s6 = "00.....";
+
+	vector<string> grid;
+	grid.push_back(s1);
+	grid.push_back(s2);
+	grid.push_back(s3);
+	grid.push_back(s4);
+	grid.push_back(s5);
+	grid.push_back(s6);
+
+	vector<string> result = bomberMan(3, grid);
+
+	return 0;
+}
+
