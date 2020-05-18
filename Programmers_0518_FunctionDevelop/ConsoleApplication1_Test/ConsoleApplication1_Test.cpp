7944a08723854d744b5da0f1f340d7fe97ab0c72
sanghoon23 <sanghoon23@naver.com>
2020-05-18 16:06:57 +0900

    Update FunctionDevelop
    *0518
    프로그래머스 기능개발 알고리즘

---
+++ Programmers_0518_FunctionDevelop/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,44 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds)
+{
+	vector<int> Result;
+	int Index = 0;
+	int size = progresses.size();
+	while (1)
+	{
+		int Insert = 0;
+		for (int i = Index; i < size; ++i)
+		{
+			progresses[i] += speeds[i];
+			if (progresses[i] >= 100 && Index == i)
+			{
+				++Insert;
+				++Index;
+			}
+		}
+
+		if (Insert != 0)
+		{
+			Result.push_back(Insert);
+		}
+
+		if (Index >= size)
+			break;
+	}
+
+	return Result;
+}
+
+int main()
+{
+
+	return 0;
+}
+
