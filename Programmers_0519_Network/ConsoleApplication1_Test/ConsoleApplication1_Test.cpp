406807569988bd2884793849c3dfb5a092d72517
sanghoon23 <sanghoon23@naver.com>
2020-05-21 21:30:00 +0900

    Update Network && WordChange
    *0521
    프로그래머스
    네트워크, 단어 변환 문제 업데이트

---
+++ Programmers_0519_Network/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,131 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+
+//@Disjoint Set 방법
+#pragma region DisjointSet
+//
+//int Find(vector<int>& Parent, int Value)
+//{
+//	if (Value == Parent[Value])
+//		return Value;
+//	else
+//	{
+//		int Ret = Find(Parent, Parent[Value]);
+//		Parent[Value] = Ret;
+//		return Ret;
+//	}
+//}
+//
+//bool Marge(vector<int>& Parent, int A, int B)
+//{
+//	A = Find(Parent, A);
+//	B = Find(Parent, B);
+//
+//	if (A != B)
+//	{
+//		Parent[B] = A;
+//		return true;
+//	}
+//
+//	return false;
+//}
+//
+//int solution(int n, vector<vector<int>> computers)
+//{
+//	int Result = n;
+//
+//	vector<int> Parent;
+//	for (int t = 0; t < n; ++t)
+//	{
+//		Parent.push_back(t);
+//	}
+//
+//	int Calc = 0;
+//	for (int j = 0; j < n; ++j)
+//	{
+//		for (int i = 0; i < n; ++i)
+//		{
+//			if (i == j) continue;
+//
+//			if (computers[j][i] == 1)
+//			{
+//				if (Marge(Parent, j, i) == true)
+//					++Calc;
+//			}
+//		}
+//	}
+//
+//	return Result - Calc;
+//}
+
+#pragma endregion
+
+
+//@DFS 방법
+#pragma region DFS
+using namespace std;
+
+bool DFS(vector<vector<int>>& Computers, int Index)
+{
+	if (Computers[Index][Index] == 0) return false;
+	Computers[Index][Index] = 0;
+
+	for (int i = 0; i < Computers.size(); ++i)
+	{
+		if (Computers[Index][i] == 1)
+			DFS(Computers, i);
+	}
+
+	return true;
+}
+
+int solution(int n, vector<vector<int>> computers)
+{
+	int Result = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (computers[i][i] == 1 && DFS(computers, i) == true)
+			++Result;
+	}
+
+	return Result;
+}
+
+#pragma endregion
+
+int main()
+{
+	vector<vector<int>> InsertVec;
+
+
+	vector<int> In1;
+	In1.push_back(1);
+	In1.push_back(1);
+	In1.push_back(0);
+
+	vector<int> In2;
+	In2.push_back(1);
+	In2.push_back(1);
+	In2.push_back(0);
+
+	vector<int> In3;
+	In3.push_back(0);
+	In3.push_back(0);
+	In3.push_back(1);
+
+	InsertVec.push_back(In1);
+	InsertVec.push_back(In2);
+	InsertVec.push_back(In3);
+
+	solution(3, InsertVec);
+
+	return 0;
+}
+
---
+++ Programmers_0521_WordChange/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,133 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <list>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+static int Result = 999999;
+
+bool Find(string& A, string& B)
+{
+	int Size = A.size();
+	int Calc = 0;
+	for (int i = 0; i < Size; ++i)
+	{
+		if (A[i] == B[i])
+			++Calc;
+	}
+
+	if (Calc == Size - 1) return true;
+	else return false;
+}
+
+#pragma region DFS
+//@DFS
+void DFS(int N, string Current, string& Target, vector<string>& Words, vector<bool>& bCheck)
+{
+	if (N >= Words.size() || Current == Target)
+	{
+		if (N < Result)
+		{
+			Result = N;
+		}
+
+		return;
+	}
+
+	for (int i = 0; i < Words.size(); ++i)
+	{
+		if (bCheck[i] == false && Find(Current, Words[i]) == true)
+		{
+			bCheck[i] = true;
+			DFS(N + 1, Words[i], Target, Words, bCheck);
+
+			//***
+			//@False 를 해주어야 기존의 bCheck 값이 유지되서 다음 I 번째에 들어감.
+			bCheck[i] = false;
+
+		}
+	}
+}
+#pragma endregion
+
+#pragma region BFS
+//@BFS
+int BFS(int N, string Current, string& Target, vector<string>& Words, vector<bool>& bCheck)
+{
+	queue<pair<string, int>> que;
+	que.push({ Current, N });
+
+	int Ret = 0;
+	while (!que.empty())
+	{
+		string StrCurr = que.front().first;
+		int Value = que.front().second;
+		que.pop();
+
+		if (StrCurr == Target)
+		{
+			Ret = Value;
+			break;
+		}
+
+		for (int i = 0; i < Words.size(); ++i)
+		{
+			if (bCheck[i] == false && Find(StrCurr, Words[i]) == true)
+			{
+				bCheck[i] = true;
+				que.push({ Words[i], Value + 1 });
+			}
+
+		}
+	}
+
+	return Ret;
+}
+
+#pragma endregion
+
+int solution(string begin, string target, vector<string> words)
+{
+	int Size = words.size();
+	vector<bool> bCheck(Size, false);
+
+	//@DFS
+	//for (int j = 0; j < Size; ++j)
+	//{
+	//	if (Find(begin, words[j]) == true)
+	//	{
+	//		std::fill(bCheck.begin(), bCheck.end(), false);
+	//		bCheck[j] = true;
+
+	//		DFS(0, words[j], target, words, bCheck);
+	//	}
+	//}
+	//if (Result == 999999) return 0;
+
+	//@BFS
+	Result = BFS(0, begin, target, words, bCheck);
+
+	return Result;
+}
+
+int main()
+{
+	string Begin = "hit";
+	string Target = "cog";
+
+	vector<string> Input;
+	Input.push_back(string("hot"));
+	Input.push_back(string("dot"));
+	Input.push_back(string("dog"));
+	Input.push_back(string("lot"));
+	Input.push_back(string("log"));
+	Input.push_back(string("cog"));
+
+	int Result = solution(Begin, Target, Input);
+
+	return 0;
+}
+
