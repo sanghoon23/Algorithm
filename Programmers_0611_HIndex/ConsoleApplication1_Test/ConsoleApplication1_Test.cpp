7cc254762027343b2a89aa4ec0a68be35d6a4195
sanghoon23 <sanghoon23@naver.com>
2020-06-11 16:14:37 +0900

    Update
    *0611
    HIndex

---
+++ Programmers_0611_HIndex/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,42 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+bool Compare(int a, int b)
+{
+	return a > b;
+}
+
+int solution(vector<int> citations)
+{
+	sort(citations.begin(), citations.end(), Compare);
+
+	int Max = 0;
+	int Count = 0;
+	for (int i = 10000; i >= 0; --i)
+	{
+		if (i > citations.size()) continue;
+		Max = i;
+		Count = 0;
+		for (int j = 0; j < citations.size(); ++j)
+		{
+			if (citations[j] >= Max) ++Count;
+			else break;
+		}
+
+		if (Count >= Max) break;
+	}
+
+	return Max;
+}
+
+int main()
+{
+
+	return 0;
+}
+
