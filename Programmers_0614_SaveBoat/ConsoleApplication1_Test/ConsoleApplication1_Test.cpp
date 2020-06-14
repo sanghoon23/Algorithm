837d16a3a79f711665df63f7b69f09175ed5b18c
sanghoon23 <sanghoon23@naver.com>
2020-06-14 22:30:47 +0900

    Update
    *0614
    SaveBoat
    구명 보트 태우기 문제,

---
+++ Programmers_0614_SaveBoat/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,47 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+bool Compare(int a, int b)
+{
+	return a > b;
+}
+
+int solution(vector<int> people, int limit)
+{
+	sort(people.begin(), people.end(), Compare);
+	int Count = people.size();
+	int j = people.size();
+	for (int i = 0; i < people.size(); ++i)
+	{
+		if (i >= j - 1) break;
+
+		if (people[i] + people[j - 1] <= limit)
+		{
+			--j;
+			--Count;
+		}
+	}
+
+	return Count;
+}
+
+int main()
+{
+	vector<int> Input;
+	Input.push_back(70);
+	Input.push_back(50);
+	Input.push_back(80);
+	Input.push_back(50);
+
+	int Limit = 100;
+
+	int Result = solution(Input, Limit);
+
+	return 0;
+}
+
