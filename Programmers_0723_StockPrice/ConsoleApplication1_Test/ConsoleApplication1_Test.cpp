55684a45a0a5f3b83b8670cc54a1791b60e2c64c
sanghoon23 <sanghoon23@naver.com>
2020-07-23 17:05:24 +0900

    Update
    *0723
    StockPrice 주식 가격.

---
+++ Programmers_0723_StockPrice/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices)
+{
+	vector<int> Answer;
+
+	for (int i = 0; i < prices.size(); ++i)
+	{
+		for (int j = i + 1; j < prices.size(); ++j)
+		{
+			if (prices[i] > prices[j])
+			{
+				Answer.push_back(j - i);
+				break;
+			}
+
+			if (j == prices.size() - 1) Answer.push_back(j - i);
+		}
+	}
+
+	Answer.push_back(0);
+	return Answer;
+}
+
+int main()
+{
+
+	return 0;
+}
+
