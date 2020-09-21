330d81cd95d9abed3465d8fa68f46a421171fb04
sanghoon23 <sanghoon23@naver.com>
2020-09-22 00:31:58 +0900

    Update
    *0921
    프로그래머스 최고의 집합

---
+++ Programmers_0921_BestSet/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,60 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+void Check(vector<int>& Result, int N, int S)
+{
+	int Half = S / N;
+	if (Half == 0)
+	{
+		int Remain = S % N;
+		for (int i = 0; i < Remain; ++i)
+		{
+			Result[i] += 1;
+		}
+	}
+	else
+	{
+		for (int i = 0; i < N; ++i)
+		{
+			Result[i] += Half;
+		}
+		Check(Result, N, S - (N * Half));
+	}
+}
+
+vector<int> solution(int n, int s)
+{
+	vector<int> Answer;
+
+	vector<int> Result(n, 0);
+	Check(Result, n, s);
+
+	if (Result[n - 1] == 0)
+	{
+		Answer.push_back(-1);
+		return Answer;
+	}
+	else
+	{
+		Answer = Result;
+	}
+
+	return Answer;
+}
+
+int main()
+{
+	int N = 3;
+	int S = 8;
+
+	vector<int> Result = solution(N, S);
+
+	return 0;
+}
+
