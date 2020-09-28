e64c4e36d3eaf2a625c27ccf9c3822a51b6bc7cd
sanghoon23 <sanghoon23@naver.com>
2020-09-28 22:29:25 +0900

    Update
    *0928
    프로그래머스 124 나라의 숫자

---
+++ Programmers_0928_124NumberOfState/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+string Str[3] = { "1", "2", "4" };
+
+string solution(int n)
+{
+	string Answer = "";
+
+	int Temp = n;
+	while (Temp >= 0)
+	{
+		Temp -= 1;
+		if (Temp < 0) break;
+
+		int Remain = Temp % 3;
+		Temp = Temp / 3;
+
+		Answer.append(Str[Remain]);
+	}
+
+	reverse(Answer.begin(), Answer.end());
+	return Answer;
+}
+
+int main()
+{
+
+	return 0;
+}
+
