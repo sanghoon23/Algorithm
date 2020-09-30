8fdb6e05df789c8653aaa377af61e9e09c0029be
sanghoon23 <sanghoon23@naver.com>
2020-10-01 00:38:17 +0900

    Update
    *0930
    프로그래머스 올바른 괄호

---
+++ Programmers_0930_Right()/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+bool solution(string S)
+{
+	queue<int> Que;
+	for (int i = 0; i < S.size(); ++i)
+	{
+		if (S[i] == '(')
+			Que.push(1);
+		else
+		{
+			if (Que.empty()) return false;
+			Que.pop();
+		}
+	}
+
+	if (!Que.empty()) return false;
+	return true;
+}
+
+int main()
+{
+
+	return 0;
+}
+
