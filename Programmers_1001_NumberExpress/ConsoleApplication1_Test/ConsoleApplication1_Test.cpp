d0fa11529ef093a8e12431ffd0bed5a181ec748c
sanghoon23 <sanghoon23@naver.com>
2020-10-01 21:47:02 +0900

    Update
    *1001
    프로그래머스 숫자의 표현

---
+++ Programmers_1001_NumberExpress/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,42 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int n)
+{
+	int Answer = 1;
+
+	int Temp = n - 1;
+	for (int i = Temp; i >= 1; --i)
+	{
+		int Value = i, Count = i;
+		while (Count > 0)
+		{
+			--Count;
+			Value += Count;
+			if (Value == n)
+			{
+				++Answer;
+				break;
+			}
+			else if (Value > n)
+				break;
+		}
+	}
+
+	return Answer;
+}
+
+int main()
+{
+	int N = 15;
+	int Result = solution(15);
+
+	return 0;
+}
+
