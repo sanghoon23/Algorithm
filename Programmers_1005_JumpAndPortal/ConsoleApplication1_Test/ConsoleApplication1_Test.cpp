eb52e778270d548a34dd46e6e91491dfb1664100
sanghoon23 <sanghoon23@naver.com>
2020-10-05 23:01:18 +0900

    Update
    *1005
    프로그래머스 점프와 순간이동

---
+++ Programmers_1005_JumpAndPortal/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int N)
+{
+	int Answer = 0;
+
+	while (N > 0)
+	{
+		if (N == 1) { ++Answer; break; }
+		if (N % 2 == 1) { N -= 1; ++Answer; }
+
+		N /= 2;
+	}
+
+	return Answer;
+}
+
+int main()
+{
+
+	return 0;
+}
+
