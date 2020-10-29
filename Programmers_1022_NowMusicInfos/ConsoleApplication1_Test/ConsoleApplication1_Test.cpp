e80ba598a1fa1b613fdfab4e6e42c71133b0ebf3
sanghoon23 <sanghoon23@naver.com>
2020-10-29 15:23:20 +0900

    Update
    *1029
    다시 풀어보기 - (Token 화, 문자치환)
    방금 그곡

--- Programmers_1022_NowMusicInfos/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
+++ Programmers_1022_NowMusicInfos/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -3,9 +3,97 @@
 #include <string>
 #include <vector>
 #include <list>
+#include <map>
 #include <algorithm>
 
 using namespace std;
+
+//struct Music
+//{
+//	int Index = 0, PlayTime = 0;
+//	string Name = "", Sheet = "";
+//};
+//
+//int TimeCalc(string Time)
+//{
+//	int HourTime = (Time[0] - '0') * 600 + (Time[1] - '0') * 60;
+//	int MinTime = (Time[3] - '0') * 10 + (Time[4] - '0');
+//	return HourTime + MinTime;
+//}
+//
+//Music Find(string Str)
+//{
+//	Music Ret;
+//	string EndTime = Str.substr(6, 5), StartTime = Str.substr(0, 5), MusicName = "";
+//	int FIndex = 0;
+//	for (int i = 12; i < Str.size(); ++i)
+//	{
+//		if (Str[i] == ',') { FIndex = i + 1; break; }
+//		else MusicName.push_back(Str[i]);
+//	}
+//	Ret.PlayTime = TimeCalc(EndTime) - TimeCalc(StartTime);
+//	Ret.Name = MusicName;
+//	Ret.Sheet = Str.substr(FIndex, Str.size());
+//	return Ret;
+//}
+//
+//string solution(string M, vector<string> MusicInfos)
+//{
+//	vector<Music> Temp;
+//	for (int j = 0; j < MusicInfos.size(); ++j)
+//	{
+//		Music Muse = Find(MusicInfos[j]);
+//		Muse.Index = j;
+//
+//		string Sheet = "";
+//		int Idx = 0, Count = 0;
+//		while (Count <= Muse.PlayTime) //*마지막에 ABC# 일 수 있다. 그래서 # 도 포햄해줘야함. (<= 인 이유)
+//		{
+//			Sheet.push_back(Muse.Sheet[Idx % Muse.Sheet.size()]);
+//			++Idx;
+//			if (Muse.Sheet[Idx % Muse.Sheet.size()] != '#') ++Count;
+//		}
+//
+//		for (int i = 0; i < Sheet.size(); ++i)
+//		{
+//			bool bFind = true;
+//			if (M[0] == Sheet[i] && Sheet[i] != '#')
+//			{
+//				int MIndex = i;
+//				for (int k = 0; k < M.size(); ++k)
+//				{
+//					if (M[k] != Sheet[(k + i) % Sheet.size()]) { bFind = false; break; }
+//				}
+//				int LastIndex = i + M.size();
+//				if (bFind == true)
+//				{
+//					if (Sheet[(LastIndex) % Sheet.size()] != '#') { Temp.push_back(Muse); break; }
+//					else continue;
+//				}
+//			}
+//		}
+//	}
+//
+//	if(Temp.size() == 0) return "(None)";
+//	else if (Temp.size() == 1) return Temp[0].Name;
+//
+//	Music Ret = Temp[0];
+//	for (int i = 1; i < Temp.size(); ++i)
+//	{
+//		if (Temp[i].PlayTime > Ret.PlayTime) Ret = Temp[i];
+//		else if(Temp[i].PlayTime == Ret.PlayTime)
+//		{
+//			if (Temp[i].Index < Ret.Index) Ret = Temp[i];
+//		}
+//	}
+//	return Ret.Name;
+//}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//#1029_ Token 화 시켜서 다시 풀기.
+
+//string SingSheet[12] = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+pair<char, int> SingSheet[5] = { {'C',33}, {'D',34}, {'F',35}, {'G',36}, {'A',37}, };
+map<char, int> Map;
 
 struct Music
 {
@@ -15,105 +103,94 @@ struct Music
 
 int TimeCalc(string Time)
 {
-	int HourTime = (Time[0] - '0') * 600 + (Time[1] - '0') * 60;
-	int MinTime = (Time[3] - '0') * 10 + (Time[4] - '0');
-	return HourTime + MinTime;
+	return ((Time[0] - '0') * 600 + (Time[1] - '0') * 60) + ((Time[3] - '0') * 10 + (Time[4] - '0'));
+}
+
+string ChangeFlat(string Str)
+{
+	string Ret;
+	int Index = 0;
+	while(Index < Str.size())
+	{
+		if (Index + 1 < Str.size() && Str[Index + 1] == '#')
+		{
+			Ret.push_back(Map[Str[Index]]);
+			++Index;
+		}
+		else Ret.push_back(Str[Index]);
+		++Index;
+	}
+	return Ret;
 }
 
 Music Find(string Str)
 {
 	Music Ret;
-	string EndTime = Str.substr(6, 5), StartTime = Str.substr(0, 5), MusicName = "";
+	string StartTime = Str.substr(0, 5), EndTime = Str.substr(6, 5);
+	Ret.PlayTime = TimeCalc(EndTime) - TimeCalc(StartTime);
 	int FIndex = 0;
 	for (int i = 12; i < Str.size(); ++i)
 	{
 		if (Str[i] == ',') { FIndex = i + 1; break; }
-		else MusicName.push_back(Str[i]);
+		Ret.Name.push_back(Str[i]);
 	}
-	Ret.PlayTime = TimeCalc(EndTime) - TimeCalc(StartTime);
-	Ret.Name = MusicName;
-	Ret.Sheet = Str.substr(FIndex, Str.size());
+	Ret.Sheet = ChangeFlat(Str.substr(FIndex, Str.size()));
 	return Ret;
 }
 
 string solution(string M, vector<string> MusicInfos)
 {
+	for (int i = 0; i < 5; ++i)
+		Map[SingSheet[i].first] = SingSheet[i].second;
+
+	string FlatM = ChangeFlat(M);
+
 	vector<Music> Temp;
 	for (int j = 0; j < MusicInfos.size(); ++j)
 	{
 		Music Muse = Find(MusicInfos[j]);
 		Muse.Index = j;
 
-		string Sheet = "";
-		int Idx = 0, Count = 0;
-		while (Count <= Muse.PlayTime) //*마지막에 ABC# 일 수 있다. 그래서 # 도 포햄해줘야함. (<= 인 이유)
-		{
-			Sheet.push_back(Muse.Sheet[Idx % Muse.Sheet.size()]);
-			++Idx;
-			if (Muse.Sheet[Idx % Muse.Sheet.size()] != '#') ++Count;
-		}
+		string PlaySing = "";
+		for (int i = 0; i < Muse.PlayTime; ++i)
+			PlaySing.push_back(Muse.Sheet[i % Muse.Sheet.size()]);
 
-		for (int i = 0; i < Sheet.size(); ++i)
+		for (int j = 0; j < PlaySing.size(); ++j)
 		{
-			bool bFind = true;
-			if (M[0] == Sheet[i] && Sheet[i] != '#')
+			if (PlaySing[j] == FlatM[0])
 			{
-				int MIndex = i;
-				for (int k = 0; k < M.size(); ++k)
+				bool bFind = true;
+				for (int i = 1; i < FlatM.size(); ++i)
 				{
-					if (M[k] != Sheet[(k + i) % Sheet.size()]) { bFind = false; break; }
+					if (PlaySing[j + i] != FlatM[i])
+					{
+						bFind = false;
+						break;
+					}
 				}
-				int LastIndex = i + M.size();
 				if (bFind == true)
 				{
-					if (Sheet[(LastIndex) % Sheet.size()] != '#') { Temp.push_back(Muse); break; }
-					else continue;
+					Temp.push_back(Muse);
+					break;
 				}
 			}
 		}
-
-		//if (Muse.PlayTime >= Muse.Sheet.size())
-		//{
-		//	for (int i = 0; i < Sheet.size(); ++i)
-		//	{
-		//		bool bFind = true;
-		//		if (M[0] == Sheet[i])
-		//		{
-		//			int MIndex = i;
-		//			for (int k = i; k < Sheet.size() + i; ++k)
-		//			{
-		//				if (k - MIndex >= M.size()) break;
-		//				if (M[(k - MIndex) % M.size()] != Sheet[k % Sheet.size()]) { bFind = false; break; }
-		//			}
-		//			if (bFind == true) { Temp.push_back(Muse); break; }
-		//		}
-		//	}
-		//}
-		//else
-		//{
-		//	bool bFind = true;
-		//	for (int i = 0; i < Sheet.size(); ++i)
-		//	{
-		//		if (i >= M.size()) break;
-		//		if (M[i] != Sheet[i]) { bFind = false; break; }
-		//	}
-		//	if (bFind == true) { Temp.push_back(Muse); }
-		//}
 	}
 
-	if(Temp.size() == 0) return "(None)";
-	else if (Temp.size() == 1) return Temp[0].Name;
-
-	Music Ret = Temp[0];
+	if (Temp.size() == 0) return "(None)";
+	Music Result = Temp[0];
 	for (int i = 1; i < Temp.size(); ++i)
 	{
-		if (Temp[i].PlayTime > Ret.PlayTime) Ret = Temp[i];
-		else if(Temp[i].PlayTime == Ret.PlayTime)
+		if (Temp[i].PlayTime > Result.PlayTime)
+			Result = Temp[i];
+		else if (Temp[i].PlayTime == Result.PlayTime)
 		{
-			if (Temp[i].Index < Ret.Index) Ret = Temp[i];
+			if (Temp[i].Index < Result.Index)
+				Result = Temp[i];
 		}
 	}
-	return Ret.Name;
+
+	return Result.Name;
 }
 
 int main()
