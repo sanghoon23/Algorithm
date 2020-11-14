d82c4b48c863d3f52c89bf3ff3b99f19b7d8f016
sanghoon23 <sanghoon23@naver.com>
2020-11-14 18:05:25 +0900

    Update
    *1114
    프로그래머스 KAKAO [1차] 캐쉬

---
+++ Programmers_1114_Cache/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,49 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int CacheSize, vector<string> Cities)
+{
+	if (CacheSize == 0) return Cities.size() * 5;
+
+	int Answer = CacheSize * 5;
+	for (int i = 0; i < Cities.size(); ++i)
+		transform(Cities[i].begin(), Cities[i].end(), Cities[i].begin(), [](char c) { return tolower(c); });
+
+	vector<string> Temp;
+	map<string, int> Map;
+	for (int i = 0; i < CacheSize; ++i)
+	{
+		Map[Cities[i]] += 1;
+		Temp.push_back(Cities[i]);
+		if (Map[Cities[i]] >= 2) Answer -= 4;
+	}
+
+	for (int j = CacheSize; j < Cities.size(); ++j)
+	{
+		bool bFind = false;
+		int Index = 0;
+		for (int i = 0; i < Temp.size(); ++i)
+		{
+			if (Temp[i] == Cities[j]) { bFind = true; Index = i; break; }
+		}
+		if (bFind) { Temp.erase(Temp.begin() + Index); Answer += 1; }
+		else { Temp.erase(Temp.begin() + 0); Answer += 5; }
+		Temp.push_back(Cities[j]);
+	}
+
+	return Answer;
+}
+
+int main()
+{
+
+	return 0;
+}
+
