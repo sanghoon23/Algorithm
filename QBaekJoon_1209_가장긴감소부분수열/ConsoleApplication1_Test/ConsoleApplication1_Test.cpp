689f31a8c0ac9fa41ecb8bbe41db2d4f6f35de14
sanghoon23 <sanghoon23@naver.com>
2020-12-09 20:33:44 +0900

    Update
    *1209
    BaekJoon - 대표적 DP 문제

---
+++ QBaekJoon_1209_가장긴감소부분수열/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,50 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+//@2차
+int N = 0, In = 0;
+vector<int> Num;
+int main()
+{
+	cin >> N;
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> In;
+		In *= -1;
+		if (Num.empty() || !Num.empty() && Num[Num.size() - 1] < In) Num.push_back(In);
+		else Num[lower_bound(Num.begin(), Num.end(), In) - Num.begin()] = In;
+
+	}
+	cout << Num.size();
+	return 0;
+}
+
+//@1차
+//int N = 0;
+//int Value[1001] = { 0, };
+//int main()
+//{
+//	cin >> N;
+//	for (int i = 0; i < N; ++i)
+//		cin >> Value[i];
+//	
+//	reverse(Value, Value + N);
+//	vector<int> Num;
+//	for (int i = 0; i < N; ++i)
+//	{
+//		if (Num.empty() || !Num.empty() && Num[Num.size() - 1] < Value[i])
+//			Num.push_back(Value[i]);
+//		else
+//		{
+//			int Idx = lower_bound(Num.begin(), Num.end(), Value[i]) - Num.begin();
+//			Num[Idx] = Value[i];
+//		}
+//	}
+//
+//	cout << Num.size();
+//	return 0;
+//}
+
---
+++ QBaekJoon_1209_타일채우기/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int N = 0;
+int Dp[31] = { 0, };
+int main()
+{
+	cin >> N;
+	Dp[0] = 1;
+	Dp[2] = 3;
+	for (int k = 4; k <= N; ++k)
+	{
+		Dp[k] += Dp[k - 2] * 3;
+		for (int j = 4; j <= k; j += 2)
+			Dp[k] += Dp[k - j] * 2;
+	}
+	cout << Dp[N];
+	return 0;
+}
+
