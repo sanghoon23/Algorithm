cfee737b022c5a3f5887e5cc50c4c3c0d2d7f994
sanghoon23 <sanghoon23@naver.com>
2021-01-13 09:31:29 +0900

    Update
    *210113
    BeakJoon - 위상정렬

---
+++ QBaekJoon_210112_부등호/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,105 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <string.h>
+
+///////////////////////////////////////////////////////////////////////////////
+//@스택을 이용한 코드
+/*
+#include <stack>
+int k, n=9;
+char a[15];
+std::stack<int> st;
+
+void f() {
+	while(!st.empty()) printf("%d", st.top()), st.pop();
+}
+
+int main() {
+	int i;
+	scanf("%d", &k);
+	st.push(n--);
+	for(i=0; i<k; i++) {
+		scanf("%s", &a[i]);
+		if(a[i]=='>') f();
+		st.push(n--);
+	}
+	f();
+	puts("");
+	n = 0;
+	st.push(n++);
+	for(i=0; i<k; i++) {
+		if(a[i]=='<') f();
+		st.push(n++);
+	}
+	f();
+	return 0;
+}
+*/
+
+using namespace std;
+int K = 0;
+string Str, MaxStr = "", MinStr = "";
+bool Visited[10];
+void DFS(string Curr, int Idx)
+{
+	if (Idx >= Str.size())
+	{
+		if (MaxStr == "" && MinStr == "") { MaxStr = Curr; MinStr = Curr; }
+		else
+		{
+			long long R = atoll(Curr.c_str()), MaxR = atoll(MaxStr.c_str()), MinR = atoll(MinStr.c_str());
+			if (R > MaxR) MaxStr = Curr;
+			else if (R < MinR) MinStr = Curr;
+		}
+		return;
+	}
+
+	for (int i = 0; i <= 9; ++i)
+	{
+		if (Visited[i] == false)
+		{
+			if (Str[Idx] == '<' && Curr[Idx] - '0' < i)
+			{
+				Visited[i] = true;
+				string Loop = Curr;
+				Loop.push_back(i + '0');
+				DFS(Loop, Idx + 1);
+				Visited[i] = false;
+			}
+			else if (Str[Idx] == '>' && Curr[Idx] - '0' > i)
+			{
+				Visited[i] = true;
+				string Loop = Curr;
+				Loop.push_back(i + '0');
+				DFS(Loop, Idx + 1);
+				Visited[i] = false;
+			}
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(0);
+	cin >> K;
+	for (int i = 0; i < K; ++i)
+	{
+		char Input; cin >> Input;
+		Str.push_back(Input);
+	}
+
+	for (int i = 0; i <= 9; ++i)
+	{
+		memset(Visited, false, sizeof(Visited));
+		Visited[i] = true;
+		string C;
+		C.push_back(i + '0');
+		DFS(C, 0);
+	}
+
+	cout << MaxStr << '\n';
+	cout << MinStr << '\n';
+	return 0;
+}
---
+++ QBaekJoon_210112_최종순위/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,49 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+int T = 0, N = 0, K = 0;
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(0);
+	cin >> T;
+	while (T--)
+	{
+		cin >> N;
+		vector<int> Input(N + 1);
+		for (int i = 1; i <= N; ++i)
+			cin >> Input[i];
+
+		vector<vector<int>> Board(N + 1, vector<int>(N + 1));
+		for (int j = 1; j <= N; ++j)
+			for (int i = j + 1; i <= N; ++i)
+				Board[Input[j]][Input[i]] = 1;
+
+		cin >> K;
+		for (int i = 0; i < K; ++i)
+		{
+			int F, S;
+			cin >> F >> S;
+			swap(Board[F][S], Board[S][F]);
+		}
+
+		for (int j = 1; j <= N; ++j)
+			for (int i = j + 1; i <= N; ++i)
+				if (Board[Input[i]][Input[j]] == 1) swap(Input[i], Input[j]);
+
+		bool bImpossible = false;
+		for (int j = 1; j <= N; ++j)
+			for (int i = j + 1; i <= N; ++i)
+				if (Board[Input[i]][Input[j]] == 1) { bImpossible = true; }
+
+		if (bImpossible == true) printf("IMPOSSIBLE");
+		else
+		{
+			for (int k = 1; k <= N; ++k)
+				printf("%d ", Input[k]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
