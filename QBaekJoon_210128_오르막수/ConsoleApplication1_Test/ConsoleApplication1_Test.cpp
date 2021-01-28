0e23c68c51636633fd997705d55bfc8d06afd3d3
sanghoon23 <sanghoon23@naver.com>
2021-01-28 18:01:07 +0900

    Update
    *210128
    BaekJoon - DP & DP 응용, 상기하기

---
+++ QBaekJoon_210128_오르막수/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define LL long long
+#define MOD 10007
+int N;
+LL Dp[1001][10];
+LL Solve(int Idx, int Num)
+{
+	if (Idx >= N) return 1;
+	LL& Ret = Dp[Idx][Num];
+	if (Ret != -1) return Ret;
+	Ret = 0;
+	for (int k = 0; k <= 9; ++k)
+		if (Num <= k) Ret += (Solve(Idx + 1, k) % MOD);
+
+	return Ret % MOD;
+}
+
+int main()
+{
+	FASTIO;
+	cin >> N;
+	memset(Dp, -1, sizeof(Dp));
+	cout << Solve(0, 0) % MOD << '\n';
+	return 0;
+}
+
---
+++ QBaekJoon_210128_이모티콘/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,60 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string.h>
+
+using namespace std;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define MMAX 987654321
+#define PII pair<int, int>
+int S;
+int main()
+{
+	FASTIO;
+	cin >> S;
+
+	vector<vector<int>> Dist(S + 1, vector<int>(S + 1, MMAX));
+	Dist[1][0] = 0;
+	priority_queue<pair<int, PII>> PQ; //@비용, 클립, 현재화면
+	PQ.push({ 0, {0, 1} });
+	while (!PQ.empty())
+	{
+		const int Cost = -(PQ.top().first), Clip = PQ.top().second.first, Curr = PQ.top().second.second;
+		PQ.pop();
+
+		if (Curr > S || Curr < 1 || Clip > S) continue;
+		if (Dist[Curr][Clip] < Cost) continue;
+
+		if (Curr > 0 && Clip != Curr) //클립보드저장.
+		{
+			int InsertClip = Curr;
+			PQ.push({ -(Cost + 1), {InsertClip, Curr} });
+		}
+		if (Clip > 0) //화면에붙여넣기
+		{
+			if (Curr + Clip <= S && Dist[Curr + Clip][Clip] > Cost + 1)
+			{
+				Dist[Curr + Clip][Clip] = Cost + 1;
+				PQ.push({ -(Cost + 1), {Clip, Curr + Clip} });
+			}
+		}
+		if (Curr - 1 > 0) //화면하나삭제
+		{
+			int InsertCurr = Curr - 1;
+			if (InsertCurr <= S && Dist[InsertCurr][Clip] > Cost + 1)
+			{
+				Dist[InsertCurr][Clip] = Cost + 1;
+				PQ.push({ -(Cost + 1), {Clip, InsertCurr} });
+			}
+		}
+	}
+	int Answer = MMAX;
+	for (int i = 0; i <= S; ++i)
+		Answer = min(Answer, Dist[S][i]);
+
+	cout << Answer << '\n';
+	return 0;
+}
+
