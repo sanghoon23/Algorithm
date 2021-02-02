8b1c8b242f018bfe83f387d5aa07b4a05d4012d6
sanghoon23 <sanghoon23@naver.com>
2021-02-02 19:43:31 +0900

    Update
    *210202
    BaekJoon - KMP, 접미사 배열

---
+++ QBaekJoon_210202_CubeDitor/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,50 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+string Str;
+int Search(string& Str)
+{
+	int Ret = 0;
+	int StrSize = Str.size();
+	int Begin = 1, Match = 0;
+	vector<int> Pi(StrSize, 0);
+	while (Begin + Match < StrSize)
+	{
+		if (Str[Begin + Match] == Str[Match])
+		{
+			++Match;
+			Pi[Begin + Match - 1] = Match;
+			Ret = max(Ret, Match);
+
+		}
+		else
+		{
+			if (Match == 0) ++Begin;
+			else
+			{
+				Begin += Match - Pi[Match - 1];
+				Match = Pi[Match - 1];
+			}
+		}
+	}
+	return Ret;
+}
+
+int main()
+{
+	FASTIO;
+	cin >> Str;
+	int MMax = 0;
+	for (int j = 0; j < Str.size(); ++j)
+	{
+		string Sub = Str.substr(j, Str.size());
+		MMax = max(MMax, Search(Sub));
+	}
+	cout << MMax << '\n';
+	return 0;
+}
+
---
+++ QBaekJoon_210202_광고/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,43 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+int N;
+string Str;
+vector<int> Search(string& Str)
+{
+	int StrSize = Str.size();
+	int Begin = 1, Match = 0;
+	vector<int> Ret(StrSize, 0);
+	while (Begin + Match < StrSize)
+	{
+		if (Str[Begin + Match] == Str[Match])
+		{
+			++Match;
+			Ret[Begin + Match - 1] = Match;
+		}
+		else
+		{
+			if (Match == 0) ++Begin;
+			else
+			{
+				Begin += Match - Ret[Match - 1];
+				Match = Ret[Match - 1];
+			}
+		}
+	}
+	return Ret;
+}
+
+int main()
+{
+	FASTIO;
+	cin >> N; cin >> Str;
+	vector<int> Result = Search(Str);
+	cout << N - Result[N - 1] << '\n';
+	return 0;
+}
+
