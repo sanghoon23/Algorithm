d48f20ddf8c4d3bcdd0791c0bf6465ca9cf9d6db
sanghoon23 <sanghoon23@naver.com>
2021-02-15 17:57:05 +0900

    Update
    *210215
    BaekJoon - 트라이, KMP

---
+++ QBaekJoon_210215_문자열집합/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,91 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+int N, M;
+string Input;
+int main()
+{
+	FASTIO;
+	cin >> N >> M;
+	unordered_map<string, int> UnMap;
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> Input;
+		++UnMap[Input];
+	}
+	int Answer = 0;
+	for (int i = 0; i < M; ++i)
+	{
+		cin >> Input;
+		if (UnMap[Input] == 1) ++Answer;
+	}
+	cout << Answer;
+	return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+//@Trie 를 사용한 풀이
+//int N, M;
+//string Input;
+//
+//struct TrieNode
+//{
+//	TrieNode* Alpha[26];
+//	bool bFinal = false;
+//
+//	void Insert(string& Str)
+//	{
+//		int NSize = Str.size(), Idx = 0;
+//		TrieNode* Node = this;
+//		while (Idx < NSize)
+//		{
+//			int Curr = Str[Idx] - 'a';
+//			if (Node->Alpha[Curr] == nullptr)
+//				Node->Alpha[Curr] = new TrieNode();
+//			Node = Node->Alpha[Curr];
+//			++Idx;
+//		}
+//		Node->bFinal = true;
+//	}
+//
+//	bool Check(string& Str)
+//	{
+//		bool bFind = true;
+//		int NSize = Str.size(), Idx = 0;
+//		TrieNode* Node = this;
+//		while (Idx < NSize)
+//		{
+//			int Curr = Str[Idx] - 'a';
+//			if (Node->Alpha[Curr] == nullptr) { bFind = false; break; }
+//			Node = Node->Alpha[Curr];
+//			++Idx;
+//		}
+//		if (Node->bFinal == false) return false;
+//		return bFind;
+//	}
+//};
+//int main()
+//{
+//	FASTIO;
+//	cin >> N >> M;
+//	TrieNode* Root = new TrieNode();
+//	for (int i = 0; i < N; ++i)
+//	{
+//		cin >> Input;
+//		Root->Insert(Input);
+//	}
+//	int Answer = 0;
+//	for (int i = 0; i < M; ++i)
+//	{
+//		cin >> Input;
+//		if (Root->Check(Input))
+//			++Answer;
+//	}
+//	cout << Answer;
+//	return 0;
+//}
+
+
---
+++ QBaekJoon_210215_속타는저녁메뉴/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,90 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+int N;
+string T, P;
+
+vector<int> Search(string& Str)
+{
+	int NSize = Str.size();
+	vector<int> Ret(NSize);
+	int Begin = 1, Match = 0;
+	while (Begin + Match < NSize)
+	{
+		if (Str[Begin + Match] == Str[Match])
+		{
+			++Match;
+			Ret[Begin + Match - 1] = Match;
+		}
+		else
+		{
+			if (Match == 0) ++Begin;
+			else
+			{
+				Begin += Match - Ret[Match - 1];
+				Match = Ret[Match - 1];
+			}
+		}
+	}
+	return Ret;
+}
+
+int Find(string& T, string& P)
+{
+	int Ret = 0;
+	vector<int> Pi = Search(P);
+	int NT = T.size(), NP = P.size();
+	int Begin = 0, Match = 0;
+	while (Begin <= NT - NP)
+	{
+		if (Match < NP && T[Begin + Match] == P[Match])
+		{
+			++Match;
+			if (Match == NP) ++Ret;
+		}
+		else
+		{
+			if (Match == 0) ++Begin;
+			else
+			{
+				Begin += Match - Pi[Match - 1];
+				Match = Pi[Match - 1];
+			}
+		}
+	}
+	return Ret;
+}
+
+int GetGCD(int A, int B)
+{
+	if (B == 0) return A;
+	return GetGCD(B, A % B);
+}
+
+int main()
+{
+	FASTIO;
+	cin >> N;
+	char Input;
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> Input;
+		P.push_back(Input);
+	}
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> Input;
+		T.push_back(Input);
+	}
+	T = T + T;
+	T.pop_back();
+	int Upper = Find(T, P), Down = N;
+	int R = GetGCD(Upper, Down);
+	cout << Upper / R << "/" << Down / R;
+	return 0;
+}
+
