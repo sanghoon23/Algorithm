74b3812d407aae28b17d642b3043e292dfb2eb6c
sanghoon23 <sanghoon23@naver.com>
2021-02-22 20:21:27 +0900

    Update
    *210222
    BaekJoon - DP

---
+++ QBaekJoon_210222_게임개발/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,44 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string.h>
+
+using namespace std;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+int N, Cost[501], Dp[501];
+vector<int> Board[501];
+
+int Solve(int Idx)
+{
+	int& Ret = Dp[Idx];
+	if (Ret != -1) return Ret;
+
+	Ret = Cost[Idx];
+	int MMax = 0;
+	for (int Next : Board[Idx])
+		MMax = max(MMax, Solve(Next));
+
+	return Ret += MMax;
+}
+
+int main()
+{
+	FASTIO;
+	memset(Dp, -1, sizeof(Dp));
+	cin >> N;
+	for (int i = 1; i <= N; ++i)
+	{
+		cin >> Cost[i];
+		while (1)
+		{
+			int Input = 0; cin >> Input;
+			if (Input == -1) break;
+			Board[i].push_back(Input);
+		}
+	}
+	for (int i = 1; i <= N; ++i) if(Dp[i] == -1) Solve(i);
+	for (int i = 1; i <= N; ++i) cout << Dp[i] << '\n';
+	return 0;
+}
+
---
+++ QBaekJoon_210222_격자상의경로/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define LL long long
+int N, M, K;
+LL Dp[16][16];
+
+LL Solve(int Y, int X, int N, int M)
+{
+	if (Y > N || X > M) return 0;
+	if (Y == N && X == M) return 1;
+	LL& Ret = Dp[Y][X];
+	if (Ret != -1) return Ret;
+
+	Ret = 0;
+	Ret += Solve(Y + 1, X, N, M);
+	Ret += Solve(Y, X + 1, N, M);
+	return Ret;
+}
+
+int main()
+{
+	FASTIO;
+	memset(Dp, -1, sizeof(Dp));
+	cin >> N >> M >> K;
+	int F = 0, S = 0;
+	if (K > M)
+	{
+		F = (K % M != 0) ? (K / M) + 1 : (K / M);
+		S = (K % M != 0) ? K % M : M;
+	}
+	else F = 1, S = K;
+
+	if(K == 0 || N == 1 || M == 1) cout << Solve(1, 1, N, M);
+	else cout << Solve(1, 1, F, S) * Solve(F, S, N, M);
+	return 0;
+}
+
