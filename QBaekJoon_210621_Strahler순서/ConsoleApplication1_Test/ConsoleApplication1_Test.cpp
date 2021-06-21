1fcd33333c79e96e5fa3655778bc746af1e17511
sanghoon23 <sanghoon23@naver.com>
2021-06-21 16:12:51 +0900

    Update
    *210621 - 위상정렬

---
+++ QBaekJoon_210621_Strahler순서/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,72 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include <queue>
+
+using namespace std;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define PII pair<int, int>
+
+int T, K, M, P, Cost[1001], InDegree[1001];
+PII Calc[1001];
+vector<int> Board[1001];
+
+int main()
+{
+	FASTIO;
+	cin >> T;
+	while (T--)
+	{
+		cin >> K >> M >> P;
+		memset(Cost, 0, sizeof(Cost));
+		memset(InDegree, 0, sizeof(InDegree));
+		memset(Calc, 0, sizeof(Calc));
+
+		for (int i = 0; i <= M; ++i) Board[i].clear();
+
+		for (int i = 0; i < P; ++i)
+		{
+			int F, S; cin >> F >> S;
+			Board[F].push_back(S);
+			++InDegree[S];
+		}
+
+		queue<int> Que;
+		for (int i = 1; i <= M; ++i)
+		{
+			if (InDegree[i] == 0)
+			{
+				Cost[i] = 1;
+				Que.push(i);
+			}
+		}
+
+		while (!Que.empty())
+		{
+			int Idx = Que.front(); Que.pop();
+
+			for (int i = 0; i < Board[Idx].size(); ++i)
+			{
+				int NI = Board[Idx][i];
+				if (Calc[NI].first == Cost[Idx]) ++Calc[NI].second;
+				else if (Calc[NI].first < Cost[Idx])
+				{
+					Calc[NI].first = Cost[Idx];
+					Calc[NI].second = 1;
+				}
+
+				if (--InDegree[NI] == 0)
+				{
+					if (Calc[NI].second > 1) Cost[NI] = Calc[NI].first + 1;
+					else Cost[NI] = Calc[NI].first;
+
+					Que.push(NI);
+				}
+			}
+		}
+		cout << K << " " << Cost[M] << '\n';
+	}
+	return 0;
+}
+
---
+++ QBaekJoon_210621_장난감조립/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,106 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include <queue>
+
+using namespace std;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define PII pair<int, int>
+
+int N, M, InDegree[101], Cost[101];
+vector<PII> Board[101];
+
+int main()
+{
+	FASTIO;
+	cin >> N >> M;
+	for (int i = 0; i < M; ++i)
+	{
+		int F, S, W; cin >> W >> F >> S;
+		Board[W].push_back({ F, S });
+		++InDegree[F];
+	}
+
+	queue<int> Que;
+	Que.push(N);
+	Cost[N] = 1;
+	while (!Que.empty())
+	{
+		int Idx = Que.front(); Que.pop();
+
+		for (int i = 0; i < Board[Idx].size(); ++i)
+		{
+			int NI = Board[Idx][i].first, NC = Board[Idx][i].second;
+			Cost[NI] += NC * Cost[Idx];
+			if(--InDegree[NI] == 0) Que.push(NI);
+		}
+	}
+
+	for (int i = 1; i < N; ++i)
+	{
+		if (Board[i].empty()) cout << i << " " << Cost[i] << '\n';
+	}
+	return 0;
+}
+
+
+
+/*
+@삽질
+무엇이 나를 힘들게 했는가...
+왜 PII 컨테이너 Que 를 잡은거지??
+*/
+
+
+//using namespace std;
+//#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+//#define PII pair<int, int>
+//
+//int N, M, InDegree[101], Cost[101];
+//vector<PII> Board[101];
+//
+//int main()
+//{
+//	FASTIO;
+//	cin >> N >> M;
+//	for (int i = 0; i < M; ++i)
+//	{
+//		int F, S, W; cin >> W >> F >> S;
+//		Board[W].push_back({ F, S });
+//	}
+//	Cost[N] = 1;
+//
+//	for (int j = N; j > 0; --j)
+//	{
+//		sort(Board[j].begin(), Board[j].end());
+//		for (int i = 0; i < Board[j].size(); ++i)
+//		{
+//			int Idx = Board[j][i].first, Cnt = Board[j][i].second;
+//			Cost[Idx] += Cost[j] * Cnt;
+//		}
+//	}
+//
+//	//queue<PII> Que;
+//	//Que.push({ N, 1 });
+//	//while (!Que.empty())
+//	//{
+//	//	int Idx = Que.front().first, Cnt = Que.front().second;
+//	//	Que.pop();
+//
+//	//	for (int i = 0; i < Board[Idx].size(); ++i)
+//	//	{
+//	//		int NI = Board[Idx][i].first, NC = Board[Idx][i].second;
+//	//		Cost[NI] += NC * Cnt;
+//	//		Que.push({ NI, NC * Cnt });
+//	//	}
+//	//}
+//
+//	for (int i = 1; i <= N; ++i)
+//	{
+//		if (Board[i].empty()) cout << i << " " << Cost[i] << '\n';
+//		else break;
+//	}
+//	return 0;
+//}
+
