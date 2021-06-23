61094fcd82035b1575372a4b1b90e3c03cfa2cc7
sanghoon23 <sanghoon23@naver.com>
2021-06-23 16:02:42 +0900

    Update
    *210623 - 위상정렬

---
+++ QBaekJoon_210623_음악프로그램/ConsoleApplication1_Test/ConsoleApplication1_Test.cpp
@@ -0,0 +1,60 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include <queue>
+
+using namespace std;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+int N, M, InDegree[1001];
+vector<int> Board[1001];
+
+int main()
+{
+	FASTIO;
+	cin >> N >> M;
+	for (int i = 0; i < M; ++i)
+	{
+		int W, F, S; cin >> W;
+		cin >> F;
+		for (int j = 1; j < W; ++j)
+		{
+			cin >> S;
+			Board[F].push_back(S);
+			++InDegree[S];
+			F = S;
+		}
+	}
+
+	vector<int> Result;
+	queue<int> Que;
+	for (int i = 1; i <= N; ++i)
+	{
+		if (InDegree[i] == 0)
+		{
+			Que.push(i);
+			Result.push_back(i);
+		}
+	}
+
+	while (!Que.empty())
+	{
+		int Idx = Que.front(); Que.pop();
+
+		for (int i = 0; i < Board[Idx].size(); ++i)
+		{
+			if (--InDegree[Board[Idx][i]] == 0)
+			{
+				Que.push(Board[Idx][i]);
+				Result.push_back(Board[Idx][i]);
+			}
+		}
+	}
+
+	if (Result.size() != N) cout << 0 << '\n';
+	else for (int i = 0; i < Result.size(); ++i) cout << Result[i] << '\n';
+
+	return 0;
+}
+
